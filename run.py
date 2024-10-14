9990f405af6d77579bd72f3e5d90ac7f03aec182
TranshumanistAJ <AJDeutschland103@hotmail.com>
2024-10-14 14:41:01 +0000

    add the essentisals for m game rps

--- run.py
+++ run.py
@@ -1,3 +1,43 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def print_logo():
+    print("""
+
+ ######                                                                                                                   
+ #     #  ####   ####  #    #    #####    ##   #####  ###### #####      ####   ####  #  ####   ####   ####  #####   ####  
+ #     # #    # #    # #   #     #    #  #  #  #    # #      #    #    #      #    # # #      #      #    # #    # #      
+ ######  #    # #      ####      #    # #    # #    # #####  #    #     ####  #      #  ####   ####  #    # #    #  ####  
+ #   #   #    # #      #  #      #####  ###### #####  #      #####          # #      #      #      # #    # #####       # 
+ #    #  #    # #    # #   #     #      #    # #      #      #   #     #    # #    # # #    # #    # #    # #   #  #    # 
+ #     #  ####   ####  #    #    #      #    # #      ###### #    #     ####   ####  #  ####   ####   ####  #    #  ####  
+                                                                                                                          
+
+    """)
+
+
+def main_menu():
+    print_logo()
+    print('Please select from one of the following options')
+    print('-----------')
+    print('1. Play the game')
+    print('2. View the rules')
+    print('3. Reset the scores')
+    print('4. Exit')
+    print('-----------')
+    can_continue = False
+
+    while not can_continue:
+        choice = input('choose between 1 - 3 \n')
+        if choice == '1':
+            print('option 1 pressed')
+            can_continue = True
+        elif choice == '2':
+            print('option 2 pressed')
+            can_continue = True
+        elif choice == '3':
+            print('option 3 pressed')
+            can_continue = True
+        elif choice == '4':
+            exit()
+        else:
+            print('incorrect option chosen')
+
+main_menu()
